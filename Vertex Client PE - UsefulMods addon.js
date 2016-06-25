b49184d501c2c023ed914c1655ec0e768113c6c8
Vertex Client <rubenrubentje@hotmail.nl>
2016-06-25 14:20:48 +0200

    Updated Addon Library in script

--- Vertex Client PE - UsefulMods addon.js
+++ Vertex Client PE - UsefulMods addon.js
@@ -1,5 +1,33 @@
+/**
+*  Library made by peacestorm
+*  © 2016
+*/
+
 //Vertex UsefulMods addon v1.0
 //by peacestorm and LPMG
+
+/**
+* PART 1 OF THE FUNCTION LIBRARY (DON'T EDIT)
+*/
+
+var ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
+
+var Launcher = {
+	isBlockLauncher: function() {
+		return (ctx.getPackageName() == "net.zhuoweizhang.mcpelauncher" || ctx.getPackageName() == "net.zhuoweizhang.mcpelauncher.pro");
+	},
+	isToolbox: function() {
+		return ctx.getPackageName() == "io.mrarm.mctoolbox";
+	},
+	isMcpeMaster: function() {
+		return ctx.getPackageName() == "com.mcbox.pesdkb.mcpelauncher";
+	}
+};
+
+/**
+* YOUR ADDON CONTENT
+*/
+
 const ADDON_NAME = "UsefulMods";
 const ADDON_DESC = "Adds a few handy modules into Vertex Client PE.";
 const ADDON_VERSION = "1.0";
@@ -18,72 +46,6 @@ const Category = {
 /**
  *	CUSTOM MODULES
  */
-
-var VertexClientPE = {};
-
-VertexClientPE.nuker = function (x, y, z, range, mode) {
-
-	range = (range == null) ? 3 : range;
-
-	mode = (mode == null) ? "cube" : mode;
-
-	if (mode == "cube") {
-
-		for (var blockX = -range; blockX <= range; blockX++) {
-
-			for (var blockY = -range; blockY <= range; blockY++) {
-
-				for (var blockZ = -range; blockZ <= range; blockZ++) {
-
-					setTile(x + blockX, y + blockY, z + blockZ, 0);
-
-				}
-
-			}
-
-		}
-
-	}
-	if (mode == "flat") {
-
-		for (var blockX = -range; blockX <= range; blockX++) {
-
-			for (var blockY = -1; blockY <= range; blockY++) {
-
-				for (var blockZ = -range; blockZ <= range; blockZ++) {
-
-					setTile(x + blockX, y + blockY, z + blockZ, 0);
-
-				}
-
-			}
-
-		}
-
-	}
-	if (mode == "smash") {
-
-		for (var blockX = -range; blockX <= range; blockX++) {
-
-			for (var blockY = -range; blockY <= range; blockY++) {
-
-				for (var blockZ = -range; blockZ <= range; blockZ++) {
-
-					if (Block.getDestroyTime(getTile(x + blockX, y + blockY, z + blockZ)) == 0) {
-
-						setTile(x + blockX, y + blockY, z + blockZ, 0);
-
-					}
-
-				}
-
-			}
-
-		}
-
-	}
-
-}
 
 registerModule({
 	name: "AutoLevitate",
@@ -116,21 +78,80 @@ registerModule({
 	},
 	onTick: function () {
 		if (getTile(Player.getPointedBlockX(), Player.getPointedBlockY(), Player.getPointedBlockZ()) != 0) {
-			VertexClientPE.nuker(Player.getPointedBlockX(), Player.getPointedBlockY(),
-				Player.getPointedBlockZ(), 3, "cube");
+			callVertexFunction("nuker", [Player.getPointedBlockX(), Player.getPointedBlockY(),
+				Player.getPointedBlockZ(), 3, "cube"]);
 		}
 	}
 });
 
+/*
+***************************************************************************************************************
+
+Other parameters you can add to your modules are the following:
+
+getSettingsLayout: function() {
+  var settingsLayout = new LinearLayout(ctx);
+  //add extra widgets (such as buttons and texts) in here, they will be displayed in a mod's ... dialog
+  return settingsLayout;
+},
+onModDialogDismiss: function() {
+  //this will be called when an user closes a mod's ... dialog
+},
+canBypassYesCheatPlus: function() {
+  return false; //if the mod should be blocked by YesCheat+
+}
+
+If you want to store a variable in a module, simply add it like this: yourVariableName: yourVariableValue just like other parameters.
+You can call this variable from within the module using this.yourVariableName.
+
+It's still possible to use normal vars within the module's functions (onUseItem, onTick etcetera)
+
+***************************************************************************************************************
+Other functions and variables you can use in your addon are the following:
+##################################
+callVertexFunction(functionName, propArray);
+>> Example: callVertexFunction("nuker", [exampleX, exampleY, exampleZ, 3, "cube"]); <<
+^^ In this example it will call VertexClientPE.nuker without having to copy the whole function into your addon. This also works for other functions. ^^
+----------------------------------
+Launcher.isBlockLauncher();
+>> Example: Launcher.isBlockLauncher(); <<
+^^ This will return true if the user uses BlockLauncher. If not, it will return false.
+----------------------------------
+Launcher.isToolbox();
+>> Example: Launcher.isToolbox(); <<
+^^ This will return true if the user uses the Toolbox launcher. If not, it will return false.
+----------------------------------
+Launcher.isMcpeMaster();
+>> Example: Launcher.isMcpeMaster(); <<
+^^ This will return true if the user uses the MCPE Master launcher. If not, it will return false.
+##################################
+***************************************************************************************************************
+
+*/
+
 /**
- *	FUNCTION LIBRARY (DON'T EDIT)
+ *	PART 2 OF THE FUNCTION LIBRARY (DON'T EDIT)
  */
 
 function addonLoadHook() {
-	net.zhuoweizhang.mcpelauncher.ScriptManager.callScriptMethod("registerAddon", [ADDON_NAME, ADDON_DESC, ADDON_VERSION, TARGET_VERSION, modules]);
+  if(Launcher.isBlockLauncher() || Launcher.isMcpeMaster()) {
+    net.zhuoweizhang.mcpelauncher.ScriptManager.callScriptMethod("registerAddon", [ADDON_NAME, ADDON_DESC, ADDON_VERSION, TARGET_VERSION, modules]);
+  }
+  if(Launcher.isMcpeMaster()) {
+	com.mcbox.pesdk.mcpelauncher.ScriptManager.callScriptMethod("registerAddon", [ADDON_NAME, ADDON_DESC, ADDON_VERSION, TARGET_VERSION, modules]);
+	}
 }
 
 function registerModule(obj) {
 	obj.source = ADDON_NAME;
 	modules.push(obj);
+}
+
+function callVertexFunction(functionName, propArray) {
+  if(Launcher.isBlockLauncher() || Launcher.isMcpeMaster()) {
+  	net.zhuoweizhang.mcpelauncher.ScriptManager.callScriptMethod("VertexClientPE." + functionName, propArray);
+  }
+  if(Launcher.isMcpeMaster()) {
+		com.mcbox.pesdk.mcpelauncher.ScriptManager.callScriptMethod("VertexClientPE." + functionName, propArray);
+	}
 }
