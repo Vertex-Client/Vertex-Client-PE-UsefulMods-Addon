7a5115dd0a11604f6b00bac5b112c6c12d4578c1
Vertex Client <rubenrubentje@hotmail.nl>
2016-07-16 18:10:15 +0200

    Update addon library used in this addon

--- Vertex Client PE - MoreCheats addon.js
+++ Vertex Client PE - MoreCheats addon.js
@@ -1,8 +1,35 @@
+/**
+*  Library made by peacestorm
+*  © 2016
+*/
+
+/**
+* PART 1 OF THE FUNCTION LIBRARY (DON'T EDIT)
+*/
+
+var ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
+
+var Launcher = {
+	isBlockLauncher: function() {
+		return (ctx.getPackageName() == "net.zhuoweizhang.mcpelauncher" || ctx.getPackageName() == "net.zhuoweizhang.mcpelauncher.pro");
+	},
+	isToolbox: function() {
+		return ctx.getPackageName() == "io.mrarm.mctoolbox";
+	},
+	isMcpeMaster: function() {
+		return ctx.getPackageName() == "com.mcbox.pesdkb.mcpelauncher";
+	}
+};
+
+/**
+* YOUR ADDON CONTENT
+*/
+
 //Vertex MoreCheats v1.0 by LPMG and peacestorm
 const ADDON_NAME = "MoreCheats";
 const ADDON_DESC = "Adds some cheats into Vertex Client PE.";
 const ADDON_VERSION = "1.0";
-const TARGET_VERSION = "1.0";
+const TARGET_VERSION = "1.0.1";
 
 var modules = [];
 
@@ -17,27 +44,31 @@ const Category = {
 /**
  *	CUSTOM MODULES
  */
-var heal = {
+ 
+registerModule({
 	name: "Heal",
 	desc: "Heals you.",
 	category: Category.COMBAT,
 	type: "Mod",
-	isStateMod: function() {
+	isStateMod: function () {
 		return false;
 	},
-	onToggle: function() {
-	  Entity.setHealth(getPlayerEnt(), Entity.getMaxHealth(getPlayerEnt()));
+	onToggle: function () {
+		Entity.setHealth(getPlayerEnt(), Entity.getMaxHealth(getPlayerEnt()));
 	}
-};
-
-registerModule(heal);
+});
 
 /**
- *	FUNCTION LIBRARY (DON'T EDIT)
+ *	PART 2 OF THE FUNCTION LIBRARY (DON'T EDIT)
  */
 
 function addonLoadHook() {
-	net.zhuoweizhang.mcpelauncher.ScriptManager.callScriptMethod("registerAddon", [ADDON_NAME, ADDON_DESC, ADDON_VERSION, TARGET_VERSION, modules]);
+  if(Launcher.isBlockLauncher() || Launcher.isToolbox()) {
+    net.zhuoweizhang.mcpelauncher.ScriptManager.callScriptMethod("registerAddon", [ADDON_NAME, ADDON_DESC, ADDON_VERSION, TARGET_VERSION, modules]);
+  }
+  if(Launcher.isMcpeMaster()) {
+	com.mcbox.pesdk.mcpelauncher.ScriptManager.callScriptMethod("registerAddon", [ADDON_NAME, ADDON_DESC, ADDON_VERSION, TARGET_VERSION, modules]);
+}
 }
 
 function registerModule(obj) {
@@ -45,7 +76,11 @@ function registerModule(obj) {
 	modules.push(obj);
 }
 
-
-
-
-
+function callVertexFunction(functionName, propArray) {
+  if(Launcher.isBlockLauncher() || Launcher.isToolbox()) {
+  	net.zhuoweizhang.mcpelauncher.ScriptManager.callScriptMethod("VertexClientPE." + functionName, propArray);
+  }
+  if(Launcher.isMcpeMaster()) {
+	com.mcbox.pesdk.mcpelauncher.ScriptManager.callScriptMethod("VertexClientPE." + functionName, propArray);
+}
+}
